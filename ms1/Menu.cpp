7329dfedba57a09f43c24309427bddd4099012f6
Mahbod Darami <mdarami@myseneca.ca>
2025-03-11 00:15:57 -0400

    finished ms1 v1

--- ms1/Menu.cpp
+++ ms1/Menu.cpp
@@ -1,19 +1,89 @@
 /***********************************************************************
 // OOP244 Project, milestone 1: tester program
 //
-// File	menu.cpp
+// File	Menu.cpp
 // Version 1.0
 // Student Name and ID: Mahbod Darami - 117135244
 // Date 2025-03-10
 // Author Fardad
 // Description
-//I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
+// I have done all the coding by myself and only copied the code
+// that my professor provided to complete my workshops and assignments.
+//
 // Revision History
 // -----------------------------------------------------------
 // Name            Date            Reason
 //
-/////////////////////////////////////////////////////////////////
 ***********************************************************************/
 
 #include "Menu.h"
+#include "Utils.h"
+#include <cstring>
+
+namespace seneca {
+    extern Utils ut;
+    MenuItem::MenuItem(const char* content, size_t indent, size_t indentSize, int row) {
+        menu_content = nullptr;
+        menu_indent = 0;
+        menu_indentSize = 0;
+        menu_row = -1;
+        if (content && std::strlen(content) > 0 && !ut.isspace(content) &&
+            indent <= 4 && indentSize <= 4 && row < int(MaximumNumberOfMenuItems)) {
+            menu_content = ut.alocpy(content);
+            menu_indent = indent;
+            menu_indentSize = indentSize;
+            menu_row = row;
+        }}
+    MenuItem::MenuItem(const MenuItem& other) {
+        menu_content = nullptr;
+        if (other.menu_content) {
+            menu_content = ut.alocpy(other.menu_content);}
+        menu_indent = other.menu_indent;
+        menu_indentSize = other.menu_indentSize;
+        menu_row = other.menu_row;
+    }
+    MenuItem& MenuItem::operator=(const MenuItem& other) {
+        if (this != &other) {
+            delete[] menu_content;
+            menu_content = nullptr;
+            if (other.menu_content) {
+                menu_content = ut.alocpy(other.menu_content);
+            }
+            menu_indent = other.menu_indent;
+            menu_indentSize = other.menu_indentSize;
+            menu_row = other.menu_row;
+        }
+        return *this;
+    }
+
+    MenuItem::~MenuItem() {
+        delete[] menu_content;
+    }
+
+    MenuItem::operator bool() const {
+        return menu_content != nullptr && !ut.isspace(menu_content);
+    }
+
+
+    std::ostream& MenuItem::display(std::ostream& ostr) const {
+        if (*this) {
+            for (size_t i = 0; i < menu_indent * menu_indentSize; ++i) {
+                ostr << ' ';
+            }
+            if (menu_row >= 0) {
+                ostr.width(2);
+                ostr << menu_row << "- ";
+            }
+            ostr << "The Menu Item";  
+        
+        }
+        else {
+            ostr << "??????????";}
+        return ostr;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const MenuItem& item) 
+    {
+        return item.display(os);}
+
+}
--- ms1/Menu.h
+++ ms1/Menu.h
@@ -7,12 +7,40 @@
 // Date 2025-03-10
 // Author Fardad
 // Description
-//I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
+// I have done all the coding by myself and only copied the code
+// that my professor provided to complete my workshops and assignments.
+i got help from chatgpt for this : private:
+        MenuItem(const MenuItem& other);
+        MenuItem& operator=(const MenuItem& other);
+//
 // Revision History
 // -----------------------------------------------------------
 // Name            Date            Reason
 //
-/////////////////////////////////////////////////////////////////
 ***********************************************************************/
+#ifndef SENECA_MENU_H
+#define SENECA_MENU_H
 
+#include <iostream>
+#include "Utils.h"
+
+
+namespace seneca {
+    class MenuItem {
+        char* menu_content;
+        size_t menu_indent;
+        size_t menu_indentSize;
+        int menu_row;
+    private:
+        MenuItem(const MenuItem& other);
+        MenuItem& operator=(const MenuItem& other);
+    public:
+        MenuItem(const char* content, size_t indent, size_t indentSize, int row);
+        ~MenuItem();
+        operator bool() const;
+        std::ostream& display(std::ostream& ostr = std::cout) const;
+    };
+    std::ostream& operator<<(std::ostream& os, const MenuItem& item);
+}
+
+#endif 
